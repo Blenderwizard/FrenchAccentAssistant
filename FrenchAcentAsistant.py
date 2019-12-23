38522737e0a88b41cbc82ab5c85d33cb583f2a84
Jolan Rathelot <rathelotj@gmail.com>
2019-12-23 18:34:42 +0100

    Made It better

--- FrenchAcentAsistant.py
+++ FrenchAcentAsistant.py
@@ -1,7 +1,7 @@
 # Import modules
+import time
 from pynput.keyboard import Key, Controller
 from pynput import keyboard
-import time
 
 #Creates a keyboard controller
 keyControl = Controller()
@@ -11,59 +11,68 @@ lastKeys = ["",""] # Defines last 2 keysisShiftHeld = False # If shift key is be
 isShiftHeld = False # Flag if Shift is currently pressed
 isCapsHeld = False # Flag if Caps is was pressed during program runtime
 
+
+def emulate_keypress(to_press):
+    keyControl.press(Key.backspace)
+    keyControl.release(Key.backspace)
+    keyControl.press(Key.backspace)
+    keyControl.release(Key.backspace)
+    keyControl.press(to_press)
+    keyControl.release(to_press)
+
 # Function finds keys on press
 def on_press(key):
     # Get the global shift and capslock flags
     global isShiftHeld
     global isCapsHeld
-    
+
     capVar = [')','!','@','#','$','%','^','&','*','('] # number capitals
-    
+
     try:
         # Tests for if key is alphanumeric, AttributeError thrown if key is not alphanumeric
         'alphanumeric key {0} pressed'.format(key.char)
-        
+
         #Edits last 2 key pressed info, need logic for shift cases because upper no work on all characters
         lastKeys[0] = lastKeys[1]
-        if (isShiftHeld or isCapsHeld):
-            if (isShiftHeld):
+        if isShiftHeld or isCapsHeld:
+            if isShiftHeld:
                 lastKeys[1] = '{0}'.format(key.char).upper()
                 for i in range(10): # Number keys compiled
-                    if (str(i) == '{0}'.format(key.char)):
+                    if str(i) == '{0}'.format(key.char):
                         lastKeys[1] = capVar[i]
-                if ('`' == '{0}'.format(key.char)):
+                if '{0}'.format(key.char) == '`':
                     lastKeys[1] = '~'
-                elif ('-' == '{0}'.format(key.char)):
+                elif  '{0}'.format(key.char) == '-':
                     lastKeys[1] = '_'
-                elif ('=' == '{0}'.format(key.char)):
+                elif '{0}'.format(key.char) == '=':
                     lastKeys[1] = '+'
-                elif ('[' == '{0}'.format(key.char)):
+                elif '{0}'.format(key.char) == '[':
                     lastKeys[1] = '{'
-                elif (']' == '{0}'.format(key.char)):
+                elif '{0}'.format(key.char) == ']':
                     lastKeys[1] = '}'
-                elif (';' == '{0}'.format(key.char)):
+                elif '{0}'.format(key.char) == ';':
                     lastKeys[1] = ':'
-                elif ('\'' == '{0}'.format(key.char)):
+                elif '{0}'.format(key.char) == '\'':
                     lastKeys[1] = '"'
-                elif (',' == '{0}'.format(key.char)):
+                elif '{0}'.format(key.char) == ',':
                     lastKeys[1] = '<'
-                elif ('.' == '{0}'.format(key.char)):
+                elif '{0}'.format(key.char) == '.':
                     lastKeys[1] = '>'
-                elif ('/' == '{0}'.format(key.char)):
+                elif '{0}'.format(key.char) == '/':
                     lastKeys[1] = '?'
-                elif ('\\' == '{0}'.format(key.char)):
+                elif '{0}'.format(key.char) == '\\':
                     lastKeys[1] = '|'
             else:
                 lastKeys[1] = '{0}'.format(key.char).upper()
         else:
             lastKeys[1] = '{0}'.format(key.char)
-        
+
     except AttributeError:
         # Edits the shift and capslock flags, omits in lastkeypressed item
         if ('{0}'.format(key) == 'Key.caps_lock' or '{0}'.format(key) == 'Key.shift'):
-            if ('{0}'.format(key) == 'Key.caps_lock' and isCapsHeld == False):
+            if ('{0}'.format(key) == 'Key.caps_lock' and not isCapsHeld):
                 isCapsHeld = True
-            elif ('{0}'.format(key) == 'Key.shift'):
+            elif '{0}'.format(key) == 'Key.shift':
                 isShiftHeld = True
             else:
                 isCapsHeld = False
@@ -71,249 +80,89 @@ def on_press(key):
             # Edits last 2 key pressed info
             lastKeys[0] = lastKeys[1]
             lastKeys[1] = '{0}'.format(key)
-        
-    #Check keys if combo exist, then replaces with letter  
-    if(lastKeys[0] == '\''):
-        if (lastKeys[1] == 'e'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('é')
-            keyControl.release('é')
-        elif (lastKeys[1] == 'E'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('É')
-            keyControl.release('É')
-    elif(lastKeys[0] == '`'):
-        if (lastKeys[1] == 'a'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('à')
-            keyControl.release('à')
-        elif (lastKeys[1] == 'A'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('À')
-            keyControl.release('À')
-        elif (lastKeys[1] == 'e'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('è')
-            keyControl.release('è')
-        elif (lastKeys[1] == 'E'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('È')
-            keyControl.release('È')
-        elif (lastKeys[1] == 'u'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('ù')
-            keyControl.release('ù')
-        elif (lastKeys[1] == 'U'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('Ù')
-            keyControl.release('Ù')
-    elif(lastKeys[0] == ':'):
-        if (lastKeys[1] == 'e'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('ë')
-            keyControl.release('ë')
-        elif (lastKeys[1] == 'E'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('Ë')
-            keyControl.release('Ë')
-        elif (lastKeys[1] == 'i'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('ï')
-            keyControl.release('ï')
-        elif (lastKeys[1] == 'I'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('Ï')
-            keyControl.release('Ï')
-        elif (lastKeys[1] == 'u'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('ü')
-            keyControl.release('ü')
-        elif (lastKeys[1] == 'U'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('Ü')
-            keyControl.release('Ü')
-    elif(lastKeys[0] == '^'):
-        if (lastKeys[1] == 'a'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('â')
-            keyControl.release('â')
-        elif (lastKeys[1] == 'A'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('Â')
-            keyControl.release('Â')
-        elif (lastKeys[1] == 'e'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('ê')
-            keyControl.release('ê')
-        elif (lastKeys[1] == 'E'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('Ê')
-            keyControl.release('Ê')
-        elif (lastKeys[1] == 'i'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('î')
-            keyControl.release('î')
-        elif (lastKeys[1] == 'I'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('Î')
-            keyControl.release('Î')
-        elif (lastKeys[1] == 'o'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('ô')
-            keyControl.release('ô')
-        elif (lastKeys[1] == 'O'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('Ô')
-            keyControl.release('Ô')
-        elif (lastKeys[1] == 'u'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('û')
-            keyControl.release('û')
-        elif (lastKeys[1] == 'U'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('Û')
-            keyControl.release('Û')
-    elif(lastKeys[1] == '+'):
-        if (lastKeys[0] == 'a'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('æ')
-            keyControl.release('æ')
-        elif (lastKeys[0] == 'A'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('Æ')
-            keyControl.release('Æ')
-        elif (lastKeys[0] == 'o'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('œ')
-            keyControl.release('œ')
-        elif (lastKeys[0] == 'O'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('Œ')
-            keyControl.release('Œ')
-    elif(lastKeys[0] == '<'):
-        if (lastKeys[1] == '<'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('«')
-            keyControl.release('«')
-    elif(lastKeys[0] == '>'):
-        if (lastKeys[1] == '>'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('»')
-            keyControl.release('»')
-    elif(lastKeys[0] == ','):
-        if (lastKeys[1] == 'c'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('ç')
-            keyControl.release('ç')
-        elif (lastKeys[1] == 'C'):
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press(Key.backspace)
-            keyControl.release(Key.backspace)
-            keyControl.press('Ç')
-            keyControl.release('Ç')
-    
-        
+
+    #Check keys if combo exist, then replaces with letter
+    if lastKeys[0] == '\'':
+        if lastKeys[1] == 'e':
+            emulate_keypress('é')
+        elif lastKeys[1] == 'E':
+            emulate_keypress('É')
+    elif lastKeys[0] == '`':
+        if lastKeys[1] == 'a':
+            emulate_keypress('à')
+        elif lastKeys[1] == 'A':
+            emulate_keypress('À')
+        elif lastKeys[1] == 'e':
+            emulate_keypress('è')
+        elif lastKeys[1] == 'E':
+            emulate_keypress('È')
+        elif lastKeys[1] == 'u':
+            emulate_keypress('ù')
+        elif lastKeys[1] == 'U':
+            emulate_keypress('Ù')
+    elif lastKeys[0] == ':':
+        if lastKeys[1] == 'e':
+            emulate_keypress('ë')
+        elif lastKeys[1] == 'E':
+            emulate_keypress('Ë')
+        elif lastKeys[1] == 'i':
+            emulate_keypress('ï')
+        elif lastKeys[1] == 'I':
+            emulate_keypress('Ï')
+        elif lastKeys[1] == 'u':
+            emulate_keypress('ü')
+        elif lastKeys[1] == 'U':
+            emulate_keypress('Ü')
+    elif lastKeys[0] == '^':
+        if lastKeys[1] == 'a':
+            emulate_keypress('â')
+        elif lastKeys[1] == 'A':
+            emulate_keypress('Â')
+        elif lastKeys[1] == 'e':
+            emulate_keypress('ê')
+        elif lastKeys[1] == 'E':
+            emulate_keypress('Ê')
+        elif lastKeys[1] == 'i':
+            emulate_keypress('î')
+        elif lastKeys[1] == 'I':
+            emulate_keypress('Î')
+        elif lastKeys[1] == 'o':
+            emulate_keypress('ô')
+        elif lastKeys[1] == 'O':
+            emulate_keypress('Ô')
+        elif lastKeys[1] == 'u':
+            emulate_keypress('û')
+        elif lastKeys[1] == 'U':
+            emulate_keypress('Û')
+    elif lastKeys[1] == '+':
+        if lastKeys[0] == 'a':
+            emulate_keypress('æ')
+        elif lastKeys[0] == 'A':
+            emulate_keypress('Æ')
+        elif lastKeys[0] == 'o':
+            emulate_keypress('œ')
+        elif lastKeys[0] == 'O':
+            emulate_keypress('Œ')
+    elif lastKeys[0] == '<':
+        if lastKeys[1] == '<':
+            emulate_keypress('«')
+    elif lastKeys[0] == '>':
+        if lastKeys[1] == '>':
+            emulate_keypress('»')
+    elif lastKeys[0] == ',':
+        if lastKeys[1] == 'c':
+            emulate_keypress('ç')
+        elif lastKeys[1] == 'C':
+            emulate_keypress('Ç')
+
+
 
 # Function find keys on release
 def on_release(key):
     global isShiftHeld
-    
+
     # Sees if the key is released
-    if ('{0}'.format(key) == 'Key.shift'):
+    if '{0}'.format(key) == 'Key.shift':
         isShiftHeld = False
 
 
